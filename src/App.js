3a19ac7dae5b6ae6decb955f8f7e9fe966c81145
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-02 12:30:28 +0600

    add main router, and shared header and footer component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./Router/Routes/Routes";
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn btn-primary">Text Tailwind CSS</button>
+    <div data-theme="cupcake" className="max-w-screen-xl	mx-auto">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
